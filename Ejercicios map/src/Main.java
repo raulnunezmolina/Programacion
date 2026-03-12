0af0c574699782265e651496d2ce0f0023033fdb
raulnunezmolina <raulnm242@gmail.com>
2026-03-12 20:12:38 +0100

    Update Main.java

--- Ejercicios map/src/Main.java
+++ Ejercicios map/src/Main.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int opcion;
-
         do {
-
             System.out.println("\n--- MENU EJERCICIOS MAP ---");
             System.out.println("1. Paises y capitales");
             System.out.println("2. Frecuencia de palabras");
@@ -21,12 +17,9 @@ public class Main {
             System.out.println("9. Empleados ordenados");
             System.out.println("10. Sistema de votacion");
             System.out.println("0. Salir");
-
             System.out.print("Elige opcion: ");
             opcion = sc.nextInt();
-
             switch(opcion){
-
                 case 1: EjerciciosMap.ejercicio1(); break;
                 case 2: EjerciciosMap.ejercicio2(); break;
                 case 3: EjerciciosMap.ejercicio3(); break;
@@ -37,14 +30,10 @@ public class Main {
                 case 8: EjerciciosMap.ejercicio8(); break;
                 case 9: EjerciciosMap.ejercicio9(); break;
                 case 10: EjerciciosMap.ejercicio10(); break;
-
                 case 0: System.out.println("Saliendo..."); break;
-
                 default: System.out.println("Opcion no valida");
             }
-
         } while(opcion != 0);
-
         sc.close();
     }
 }
